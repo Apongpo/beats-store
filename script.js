546e6a2568f9ffba4482b92e6af8ace7f0e51a10
Apongpo <apongpo@gmail.com>
2025-08-20 20:16:20 +0100

    changed a name

---
+++ script.js
@@ -0,0 +1,11 @@
+// Sample beats data
+const beatsData = [
+    {
+        id: 1,
+        title: "AfroBeats",
+        producer: "BeatMaker Pro",
+        genre: "afrobeat",
+        price: 29.99,
+        audioUrl: "C:\\Users\\Admin\\Desktop\\beats-store\\Secret_Beat.mp3"
+        }
+    ]
